4146fef614130aa50c9377fcc4a26e31d10716c8
Nico.Gutschow <nico.gutschow@hotmail.de>
2020-02-27 13:39:08 +0100

    and another one

--- src/main/java/backend/FTPUploader.java
+++ src/main/java/backend/FTPUploader.java
@@ -1,14 +1,59 @@
 package backend;
 
-public class FTPUploader {
-    private String username;
-    private String password;
-    private String fileToUpload;
+import java.io.*;
 
-    public void connectToFTP() {
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+public class FTPUploader {
+
+    FTPClient ftpClient = new FTPClient();
+
+    public void ftpConnect() {
+        String server = "borrmann-shop.de";
+        int port = 21;
+        String user = "christian.preis@borrmann.de";
+        String pass = "Moku1410";
+
+        try {
+
+            ftpClient.connect(server, port);
+            ftpClient.login(user, pass);
+            ftpClient.enterLocalPassiveMode();
+
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            try {
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
+            } catch (IOException x) {
+                x.printStackTrace();
+            }
+            ex.printStackTrace();
+        }
     }
 
     public void uploadFile() {
+        try {
+            File firstLocalFile = new File("C:\\Users\\nico.gutschow\\Desktop\\pim.xml");
+
+            String firstRemoteFile = "work/pim.xml";
+            InputStream inputStream = new FileInputStream(firstLocalFile);
+
+            System.out.println("Start uploading file");
+
+            boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
+            inputStream.close();
+            if (done) {
+                System.out.println("The file was uploaded successfully.");
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
 
     }
 }
--- src/main/java/frontend/Driver.java
+++ src/main/java/frontend/Driver.java
@@ -1,10 +1,21 @@
 package frontend;
 
+import backend.FTPUploader;
+
+import java.io.IOException;
+
 public class Driver {
 	public static void main(String[] args) {
+		/*
 		Controller controller = new Controller();
 		controller.printWelcome();
 		controller.updateView();
 		controller.setModelDetails();
+		*/
+
+		FTPUploader ftpUploader = new FTPUploader();
+		ftpUploader.ftpConnect();
+		ftpUploader.uploadFile();
 	}
+
 }
